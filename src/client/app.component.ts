0d6d0b7d828e80dd1ed1663cfa3f4ac67847cfe5
Ryan de Gruyter <degruyter.ryan@gmail.com>
2016-08-29 17:06:11 +0200

    start my first service

--- src/client/app.component.ts
+++ src/client/app.component.ts
@@ -13,15 +13,11 @@ import {InfoBoxData} from "./my-material/components/infobox.component";
     `]
 })
 export class AppComponent implements OnInit {
-    private wordInfo: InfoBoxData;
+
     public onSelected = (title: string)=> {
         alert(title);
     };
 
     ngOnInit(): void {
-        this.wordInfo = {
-            title: 'Metaphor',
-            description: 'an expression, often found in literature, that describes a person or object by referring to something that is considered to have similar characteristics to that person or object'
-        };
     }
 }
