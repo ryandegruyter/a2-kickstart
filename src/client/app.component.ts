873dce4ba6f02d503cc2ceb567ffa1efa3efb389
Ryan de Gruyter <degruyter.ryan@gmail.com>
2016-08-29 13:36:10 +0200

    solution

--- src/client/app.component.ts
+++ src/client/app.component.ts
@@ -1,1 +1,13 @@
 import {Component} from '@angular/core';
+@Component({
+    selector: `my-app`,
+    template: `<h1>Hello world</h1>`,
+    styles: [
+        `h1{
+            color:red;
+        }`
+    ]
+})
+export class AppComponent{
+
+}
--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,2 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {AppModule} from "./client/app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
